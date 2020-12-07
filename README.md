0b0422b108d25b58ab4438da892b148bb6836c4a
Raphael Liere <raphael.liere@hotmail.fr>
2020-12-07 11:53:44 +0100

    Quest n°13 OK

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 Link for the demo of the application :
 
-https://www.loom.com/share/80a92e4a3fee47b89aca647bde61c78f
+https://www.loom.com/share/c98f36ab6d17427d8660c273531c3471
--- src/Entity/Program.php
+++ src/Entity/Program.php
@@ -6,9 +6,12 @@ use App\Repository\ProgramRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=ProgramRepository::class)
+ * @UniqueEntity("title", message="Ce titre existe déjà")
  */
 class Program
 {
@@ -21,11 +24,14 @@ class Program
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank(message="Champ obligatoire")
+     * @Assert\Length(max="255", maxMessage="Le titre du programme ne doit pas faire plus de 255 caractères")
      */
     private $title;
 
     /**
      * @ORM\Column(type="text")
+     * @Assert\NotBlank(message="Champ obligatoire")
      */
     private $summary;
 
